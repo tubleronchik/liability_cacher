d33fb46f8a681aee6866368cbfcbf5daee1911c6
tubleronchik <m.bystramovich@gmail.com>
2021-11-12 18:08:45 +0300

    update nix channel

--- release.nix
+++ release.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import (builtins.fetchTarball https://github.com/airalab/airapkgs/archive/bd8deed59bfaf2b67496b2071876139013926976.tar.gz)
+{ nixpkgs ? import (builtins.fetchTarball https://github.com/airalab/airapkgs/archive/nixos-unstable.tar.gz)
 , system ? builtins.currentSystem
 }:
 
